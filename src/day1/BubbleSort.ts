aa7e8e71f89a692630019f11be6b3e78d631d9cd
saad <saadgfx97@gmail.com>
2023-08-07 14:36:25 +0500

    bubble sort done

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,28 @@
 export default function bubble_sort(arr: number[]): void {
-
+  let completelySorted = false
+  let iterations = 0
+  do {
+    iterations++
+    let sortedSomeElement = false
+    for (let i = 0; i < arr.length-1; i++) {
+      if(arr[i] > arr[i+1]) {
+       let temp = arr[i]
+       arr[i] = arr[i+1]
+       arr[i+1] = temp
+       
+       sortedSomeElement = true
+      }
+      
+      if(!sortedSomeElement) completelySorted = true
+    }
+    
+  } while (!completelySorted);
+  
+  console.log({iterations})
 }
+
+const arr = [9, 3, 7, 4, 69, 420, 42];
+bubble_sort(arr)
+console.log(arr)
+
+// [3, 4, 7, 9, 42, 69, 420]
