ec46b6fc9410ad83a77f1cde04795c073119effe
saad <saadgfx97@gmail.com>
2023-08-07 09:27:01 +0500

    my bs

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -9,10 +9,8 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     i+=1
     mid = Math.floor((left+right)/2)
     
-    console.log({left, right, mid})
-    
    if(haystack[mid] == needle) {
-    console.log('here:', {mid, item: haystack[mid]})
+    console.log({size: haystack.length, i})
     return true
    }
    
@@ -23,15 +21,14 @@ export default function bs_list(haystack: number[], needle: number): boolean {
    }
   }
   
-  console.log({size: haystack.length, i})
   return false
 }
 
-bs_list([1], 100)
-bs_list([0, 1, 2], 100)
-bs_list([0, 1, 2, 3, 4, 5, 6], 100)
-bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 100)
-bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], 100)
+bs_list([1], 1)
+bs_list([0, 1, 2], 2)
+bs_list([0, 1, 2, 3, 4, 5, 6], 6)
+bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 14)
+bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], 30)
 
 /**
  * 1-1
