12379028a89190390e9e82e4460356f349cdb04e
saad <saadgfx97@gmail.com>
2023-08-07 10:04:54 +0500

    my bs

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -4,7 +4,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
   let lo = 0
   let hi = haystack.length-1
   
-  while(lo <= hi) {
+  do {
     i+=1
     const mid = Math.floor((lo+hi)/2)
     const midValue = haystack[mid]
@@ -19,23 +19,18 @@ export default function bs_list(haystack: number[], needle: number): boolean {
    if (midValue < needle) {
     lo = mid + 1
    } else {
-    hi = mid
+    hi = mid - 1
    }
-  }
+  } while (lo <= hi)
   
   return false
 }
 
-bs_list([1], 1)
-bs_list([0, 1, 2], 2)
-bs_list([0, 1, 2, 3, 4, 5, 6], 6)
-bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 14)
-bs_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], 30)
+const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420]
 
-/**
- * 1-1
- * 3-2
- * 7-3
- * 13-4
- * 31-5
- */
+// bs_list(foo, 69)
+// bs_list(foo, 1336)
+// bs_list(foo, 69420)
+// bs_list(foo, 69421)
+// bs_list(foo, 1)
+// bs_list(foo, 0)
