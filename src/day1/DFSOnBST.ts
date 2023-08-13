2dfba54bc4162ec4576e8535ed2f9fd9196c5140
saad <saadgfx97@gmail.com>
2023-08-13 23:38:08 +0500

    DFS

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,12 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+  // n.v == needle => return true
+  if(head.value == needle) return true
+  // !n => return false
+  if(!head.value) return false
+  // n.v <= needle return dfs(n.left, needle)
+  if(head.value <= needle && head.right) return dfs(head.right, needle)
+  // needle < n.v return dfs(n.right, needle)
+  if(needle < head.value && head.left) return dfs(head.left, needle)
+  
+  return false
 }
